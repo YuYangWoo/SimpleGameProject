3991afc59472365feb379ec44809fa8a48087184
jus1915 <jus1915@gmail.com>
2020-08-16 10:37:48 +0900

    윤상

--- app/src/main/java/com/cookandroid/simplegameproject/ReactGameAgainActivity.kt
+++ app/src/main/java/com/cookandroid/simplegameproject/ReactGameAgainActivity.kt
@@ -3,30 +3,34 @@ package com.cookandroid.simplegameproject
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.MotionEvent
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.react_game_main.*
 
-class ReactGameMainActivity : AppCompatActivity() {
+class ReactGameAgainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.react_game_main)
+        setContentView(R.layout.react_game_again)
 
-        button.setOnClickListener{
-            startActivity(Intent(this, ReactGameStartActivity::class.java))
-        }
-        // ActionBar Home 버튼 Enable
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         // ActionBar Title 변경
         supportActionBar?.setTitle("반응속도게임")
     }
-    // ActionBar ItemSelected 이벤트
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             android.R.id.home -> {
-                onBackPressed()
+                startActivity(Intent(this,ReactGameMainActivity::class.java))
+                finish()
                 return true
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+    override fun onBackPressed() {
+        startActivity(Intent(this,ReactGameMainActivity::class.java))
+    }
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        startActivity(Intent(this,ReactGameStartActivity::class.java))
+        finish()
+        return super.onTouchEvent(event)
     }
 }
